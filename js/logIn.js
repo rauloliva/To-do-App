1b09e133cffc237a5a6b931379989400aa5ee1bf
Raul Oliva <oliva.raul12@gmail.com>
2019-05-07 08:30:12 -0500

    Adding the redMarked function in login.js

--- js/logIn.js
+++ js/logIn.js
@@ -1,14 +1,26 @@
+var username = ""
+var password = ""
+
 //on submit
 document.querySelector("#login").addEventListener('click',function(event){
-    const username = document.querySelector('#username').value
-    const password = document.querySelector('#password').value
+    username = document.querySelector('#username').value
+    password = document.querySelector('#password').value
     if (username === "" || password === "") {
         var modal = new Modal_Content('Empty Fields','There are empty fields')
         modal.clearTexts()
         modal.setTexts()
         $("#incorrect_passwords").modal('show')
         event.preventDefault()    
+        redMarked()
     }else{
         window.location = 'dashboard.html'
     }
 })
+
+function redMarked(){
+    const inputUser = document.querySelector("#username")
+    const inputPwd = document.querySelector("#password")
+    
+    inputUser.setAttribute('style',`border:1px solid ${username === "" ? "red" : "blue"}`)
+    inputPwd.setAttribute('style',`border:1px solid ${password === "" ? "red" : "blue"}`)
+}
