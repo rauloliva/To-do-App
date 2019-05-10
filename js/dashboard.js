b5b8e9e0b3b8f9096f451aefd1228fd02232e95a
Raul Oliva <oliva.raul12@gmail.com>
2019-05-10 12:24:45 -0500

    Finishing the event keyup at dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,12 +1,40 @@
-document.querySelector('.search_box').addEventListener('keyup',function(){
+const children = [], states = []
+const father = document.querySelector('#lists')
+function getChildren() {
+    for (const kid of father.children) {
+        children.push(kid)   
+        states.push("on")
+    }
+}
+getChildren()
+
+document.querySelector('.search_box').addEventListener('keyup',function(key){
     const search_box = this.value.trim()
     if(search_box !== ""){
-        const father = document.querySelector('.container-lists').children
-        for (const child of father) {
-            const element = child.id
-            if(element === "" || element === "add") continue
-            
+        for (let i = 0; i < children.length; i++) {
+            var pattern = ""
+            for (const char of search_box) {
+                pattern += `[${char}]{1}`
+            }
+
+            var exp = new RegExp(pattern)
+            match = exp.test(children[i].id)
+            if(!match){
+                if(states[i] === "on"){
+                    states[i] = "off"
+                    document.getElementById(children[i].id).remove()
+                }
+            }else{
+                if(states[i] === "off"){
+                    father.appendChild(children[i])
+                }
+            }
         }
-        
+    }else{
+        for (let i = 0; i < children.length; i++) {
+            if(states[i] === "off"){
+                father.appendChild(children[i])
+            }
+        }
     }
 })
