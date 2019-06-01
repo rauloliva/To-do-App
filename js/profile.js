aafd9fd531028a96cce504a5c443c4443593dd3e
Raul Oliva <oliva.raul12@gmail.com>
2019-05-31 23:06:20 -0500

    Adding MVC design pattern

--- js/profile.js
+++
@@ -1,3 +0,0 @@
-document.querySelector('#select_photo').addEventListener('change',function(){
-    document.querySelector('#photo').src = (window.URL ? URL : webkitURL).createObjectURL(this.files[0]);
-})
